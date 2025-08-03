378b3f47b6bccaaa819b4050b53fe277235e129c
Amanuel Eyasu <amaneyasu2@gmail.com>
2025-08-03 07:16:46 -0700

    small change

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import Counter from "./components/Counter";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
 import Home from "./components/Home";
 import Card from "./components/Card";
+import Error from "./components/Error";
 
 function App() {
   const router = createBrowserRouter([
-    { path: "/", element: <Home /> },
+    { path: "/", element: <Home />,errorElement:<Error/> },
     {
       path: "/redux",
+      errorElement: <Error />,
       element: <Card />,
       children: [{ path: "/redux", element: <Counter /> }],
     },
